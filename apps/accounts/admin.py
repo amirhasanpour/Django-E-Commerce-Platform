3ea74cd7bebed0ecc7550d64790d7eb64d9254cf
amirhp <amirhasanpour.hp10@gmail.com>
2024-05-10 18:54:05 +0330

    Implementation forms for admin panel

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    
+    list_display = ('mobile_number', 'email', 'name', 'family', 'gender', 'is_active', 'is_admin')
+    list_filter = ('is_active', 'is_admin', 'family')
+    
+    fieldsets=(
+        (None, {'fields': ('mobile_number', 'password')}),
+        ('personal info', {'fields': ('email', 'name', 'family', 'gender', 'active_code')}),
+        ('Permissions', {'fields': ('is_active', 'is_admin')}),
+    )
+    
+    add_fieldsets=(
+        (None, {'fields': ('mobile_number', 'email', 'name', 'family', 'gender', 'password1', 'password2')}),
+    )
+    
+    search_fields = ('mobile_number',)
+    ordering = ('mobile_number',)
+    filter_horizontal = ()
+    
+    
+admin.site.register(CustomUser, CustomUserAdmin)
