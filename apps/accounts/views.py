46d6f7d0495be635206447d6655272b891410fd3
amirhp <amirhasanpour.hp10@gmail.com>
2024-05-13 00:01:46 +0330

    Implementation password forgetting and SMS panel

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,7 +3,7 @@ from django.http import HttpRequest
 from django.http.response import HttpResponse as HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import RegisterUserForm, VerifyRegisterForm, LoginUserForm
+from .forms import RegisterUserForm, VerifyRegisterForm, LoginUserForm, ChangePasswordForm, RememberPasswordForm
 import utils
 from .models import CustomUser
 from django.contrib import messages
@@ -37,6 +37,7 @@ class RegisterUserView(View):
             request.session['user_session'] = {
                 'active_code': str(active_code),
                 'mobile_number': data['mobile_number'],
+                'remember_password': False,
             }
             messages.success(request, 'کد فعال سازی ارسال شد', 'success')
             return redirect('accounts:verify')
@@ -62,11 +63,14 @@ class VerifyRegisterCodeView(View):
             user_session = request.session['user_session']
             if data['active_code'] == user_session['active_code']:
                 user = CustomUser.objects.get(mobile_number=user_session['mobile_number'])
-                user.is_active = True
-                user.active_code = utils.create_random_code(5)
-                user.save()
-                messages.success(request, 'ثبت نام با موفقیت انجام شد', 'success')
-                return redirect('main:index')
+                if user_session['remember_password'] == False:
+                    user.is_active = True
+                    user.active_code = utils.create_random_code(5)
+                    user.save()
+                    messages.success(request, 'ثبت نام با موفقیت انجام شد', 'success')
+                    return redirect('accounts:login')
+                else:
+                    return redirect('accounts:change_password')
             else:
                 messages.error(request, 'کد فعالسازی وارد شده اشتباه می باشد', 'danger')
                 return render(request, self.template_name, {'form': form})
@@ -124,7 +128,66 @@ class LogoutUserView(View):
         return redirect('main:index')
     
     
+class ChangePasswordView(View):
+    template_name = "accounts_app/change_password.html"
+    
+    def get(self, request, *args, **kwargs):
+        form = ChangePasswordForm()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = ChangePasswordForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user_session = request.session['user_session']
+            user = CustomUser.objects.get(mobile_number=user_session['mobile_number'])
+            user.set_password(data['password1'])
+            user.active_code = utils.create_random_code(5)
+            user.save()
+            messages.success(request, 'رمز عبور شما با موفقیت تغییر کرد', 'success')
+            return redirect('accounts:login')
+        else:
+            messages.error(request, 'اطلاعات وارد شده معتبر نمی باشد', 'danger')
+            return render(request, self.template_name, {'form': form})
+    
+    
+class RememberPasswordView(View):
+    template_name = "accounts_app/remember_password.html"
+    
+    def get(self, request, *args, **kwargs):
+        form = RememberPasswordForm()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = RememberPasswordForm(request.POST)
+        if form.is_valid():
+            try:
+                data = form.cleaned_data
+                user = CustomUser.objects.get(mobile_number=data['mobile_number'])
+                if user.is_admin == False:
+                    active_code = utils.create_random_code(5)
+                    user.active_code = active_code
+                    user.save()
+                    utils.send_sms(data['mobile_number'], f'کد تایید شماره موبایل شما {active_code} می باشد')
+                    request.session['user_session'] = {
+                        'active_code': str(active_code),
+                        'mobile_number': data['mobile_number'],
+                        'remember_password': True,
+                    }
+                    messages.success(request, 'جهت تغییر رمز عبور، کد دریافتی خود را ارسال کنید')
+                    return redirect('accounts:verify')
+                else:
+                    messages.error(request, 'کاربر ادمین نمی تواند از اینجا وارد شود', 'warning')
+                    return render(request, self.template_name, {'form': form})
+            except:
+                messages.error(request, 'شماره وارد شده موجود نمی باشد', 'danger')
+                return render(request, self.template_name, {'form': form})
+
+
+
 class UserPanelView(LoginRequiredMixin, View):
     template_name = "accounts_app/userpanel.html"
+    
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        return render(request, self.template_name)          
+    
--- utils.py
+++ utils.py
@@ -4,5 +4,11 @@ def create_random_code(count):
     return random.randint(10**count, 10**(count+1)-1)
 
 
+# import ghasedakpack
 def send_sms(mobile_number, messages):
     pass
+    # try:
+    #     sms = ghasedakpack.Ghasedak("64b028bdcfea1517740d4c4938ca35ae8242105d9600f85aac1a9268648aefa6")
+    #     sms.verification({'receptor': str(mobile_number),'type': '1','template': 'randcode','param1': str(messages)})
+    # except Exception as error:
+    #     print(f"Error is : {error}")
